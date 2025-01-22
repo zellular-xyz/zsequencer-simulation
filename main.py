b2f19e0003573ac46a3e15c25d5bdc261ac2758c
Ali Moosavi <amoosavi41@gmail.com>
2025-01-22 18:36:13 +0330

    Feat: Main simulation

---
+++ main.py
@@ -0,0 +1,5 @@
+from simulations.dynamic_network_simulation_with_nodes_registry import simulate_dynamic_network
+# from simulations.dispute_and_switch_simulations import simulate_dispute_and_switch
+
+if __name__ == '__main__':
+    simulate_dynamic_network()
