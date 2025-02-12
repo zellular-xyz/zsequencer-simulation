9bf85673ba78be23aff7a023db56c4b346208741
abramsymons <abramsymons@gmail.com>
2025-02-12 19:56:06 +0330

    add app dir to uvicorn

--- simulations/utils.py
+++ simulations/utils.py
@@ -48,10 +48,9 @@ def generate_node_proxy_execution_command(port, workers) -> str:
     parent_dir: str = os.path.dirname(script_dir)
     os.chdir(parent_dir)
 
-    proxy_runner_path = 'proxy.proxy_server'
+    proxy_runner_path = 'proxy_server'
     server_app = 'app'
-
-    return f"cd {config.ZSEQUENCER_PROJECT_ROOT} && source {config.ZSEQUENCER_PROJECT_VIRTUAL_ENV} && uvicorn {proxy_runner_path}:{server_app} --host 0.0.0.0 --port {port} --workers {workers}"
+    return f"cd {config.ZSEQUENCER_PROJECT_ROOT} && source {config.ZSEQUENCER_PROJECT_VIRTUAL_ENV} && uvicorn --app-dir proxy {proxy_runner_path}:{server_app} --host 0.0.0.0 --port {port} --workers {workers}"
 
 
 def delete_directory_contents(directory):
